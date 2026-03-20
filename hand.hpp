ee8e3a7367f57c63e1e9f35bea694d452e5ededf
Bao Phung <bao.phungus.26@gmail.com>
2026-03-19 21:42:01 -0700

    Updated hand.hpp to add one_pair() for sorting demonstration

--- hand.hpp
+++ hand.hpp
@@ -13,6 +13,8 @@ private:
     void sort_by_rank(Card*) const;   // sorts the hand by rank using a copy of the original hand (the hand itself should stay the same)
 
     // Add missing functions below
+    bool one_pair() const;
+
 public:
     // Included constructor with default argument.  Defaults to 5 if no argument is provided.  
     Hand(int = 5);     // default to the hand holding a maximum of 5 cards
